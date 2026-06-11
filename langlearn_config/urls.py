8ee3e4d0f2d7b4688a3743db90243bdc4551521d
Tatia dwi ersya <tiadwers@gmail.com>
2026-06-11 12:11:08 +0800

    fix: update static file handling and deployment build script

--- langlearn_config/urls.py
+++ langlearn_config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -8,3 +10,6 @@ urlpatterns = [
     path('', include('core.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
