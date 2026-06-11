ebf5264ed6c5786945f60e7c00f65f3d39643029
Tatia dwi ersya <tiadwers@gmail.com>
2026-06-11 15:44:43 +0800

    fix routing

--- langlearn_config/settings.py
+++ langlearn_config/settings.py
@@ -86,10 +86,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'core' / 'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 WHITENOISE_MANIFEST_STRICT = False
 
